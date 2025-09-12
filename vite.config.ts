246a1788edcbfba3de0b9cda4cd4e0524367e0e0
Karl Steinmann <steinmann@vakutec.at>
2025-09-12 06:00:53 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { "@": resolve(__dirname, "src") } },
-  server: { 
-    host: true, 
-    port: 5173,
-    allowedHosts: [".gitpod.io"] // alle Gitpod-Hosts erlauben
-  },
+  server: { host: true, port: 5173,strictPort: true, allowedHosts: [".gitpod.io"] },
   preview: { host: true, port: 5173 },
+  build: {
+    rollupOptions: {
+      input: {
+        admin: resolve(__dirname, "admin.html"),
+        qr: resolve(__dirname, "qr.html"),
+      },
+    },
+  },
 });
 
